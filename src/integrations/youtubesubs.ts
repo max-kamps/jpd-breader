ca894694bf88fd096219db88cf2f188fdf269691
xyaman <dbarriapaiva@gmail.com>
2023-04-29 17:48:35 -0400

    Add initial support for Youtube subtitles.
    This commit introduces support for displaying Youtube subtitles on
    the transcript panel.

---
+++ src/integrations/youtubesubs.ts
@@ -0,0 +1,21 @@
+// @reader content-script
+
+import { showError } from '../util.js';
+import { addedObserver, parseVisibleObserver } from './common.js';
+
+try {
+    const visible = parseVisibleObserver();
+
+    const added = addedObserver('yt-formatted-string.ytd-transcript-segment-renderer', elements => {
+        for (const element of elements) {
+            visible.observe(element);
+        }
+    });
+
+    added.observe(document.body, {
+        subtree: true,
+        childList: true,
+    });
+} catch (error) {
+    showError(error);
+}
