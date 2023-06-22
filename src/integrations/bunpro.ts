2a4d7f43b982c1c573bfea1d1fd34ac509d3348c
7w1 <nathanlr888@outlook.com>
2023-06-21 20:51:19 -0500

    bunpro integration

--- src/integrations/bunpro.ts
+++ src/integrations/bunpro.ts
@@ -6,8 +6,13 @@ import { addedObserver, parseVisibleObserver } from './common.js';
 try {
     const visible = parseVisibleObserver();
 
-    const added = addedObserver('.text-center', elements => {
-        for (const element of elements) visible.observe(element);
+    const added = addedObserver('div.bp-quiz-question.relative', elements => {
+        for (const element of elements) {
+            const childDiv = element.querySelector('div.text-center');
+            if (childDiv !== null && childDiv.children.length > 0) {
+                visible.observe(childDiv);
+            }
+        }
     });
 
     added.observe(document.body, {
